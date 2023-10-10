92e386203f5085a80042afefc3da609fbe9e7b52
ted-austral <eduardo.martinez@ing.austral.edu.ar>
2023-10-10 17:02:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+![Austral Ingenieria](https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQooGo7vQn4t9-6Bt46qZF-UY4_QFpYOeh7kVWzwpr_lbLr5wka)
+
+#   Project 91-vending
+
+##  Displays to simulate products on vending machine
+
+    Austral - Informatica Electronica - 2023 
+    EAMartinez
+
+    See doc/README.md for further details
+
+    
+
---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+/*
+ *  Project 91-vending 
+ *  Austral - EAM
+ *
+ *      File main.cpp
+ */
+
+#include <Arduino.h>
+
+#include "cutils.h"
+#include "wifi_ruts.h"
+#include "leds.h"
+#include "matrix.h"
+#include "stock.h"
+#include "vdisplay.h"
+
+#define FILL    99
+
+#if LED_TEST == 1
+
+static
+void
+scan_leds(unsigned val)
+{
+    int i;
+
+    Serial.printf("%s: %d\n",__FUNCTION__,val);
+    for(i = 0; i < NUM_LEDS; ++i)
+    {
+        set_led(i,val);
+        delay(500);
+    }
+}
+
+static void
+led_test(void)
+{
+    forever
+    {
+        scan_leds(1);
+        scan_leds(0);
+    }
+}
+#endif
+
+void
+setup(void)
+{
+    Serial.begin(BAUD);
+//    connect_wifi();
+
+    init_leds();
+    init_matrix();
+    init_stock();
+    init_vdisplay();
+
+#if LED_TEST == 1
+    led_test();
+#elif MATRIX_TEST == 1
+    forever
+        drawShape();
+#endif
+}
+
+void
+loop(void)
+{
+    String in;
+    int product_no,status;
+
+    status = 0;
+    do
+    {
+        Serial.printf("=========================\n");
+        refresh_vdisplay();
+        Serial.printf("Input product number (%d to %d)....", MIN_PRODUCT, MAX_PRODUCT);
+        Serial.flush();
+        while(!Serial.available())
+        ;
+        in = Serial.readStringUntil('\n');
+        product_no = atoi(in.c_str()); 
+        Serial.printf("Product selected = %d\n", product_no);
+        if( product_no == FILL )
+            Serial.printf("Refilling stock\n");
+        else if( status = (product_no < MIN_PRODUCT || product_no > MAX_PRODUCT) )
+            Serial.printf("\nBad product number %d Redo...\n", product_no );
+    } while(status);
+
+    if( product_no == FILL )
+    {
+        fill_stock();
+        Serial.printf("stock updated\n");
+        refresh_vdisplay();
+    } else
+    {
+        printf("\n\tSelected product_no = %d\n", product_no);
+        if( stock_state(product_no) > 0 )
+        {
+            Serial.printf("Stock remaining after delibering = %d\n", change_stock(product_no,-1) );
+            Serial.printf( "Product number %d delivered\n", product_no );
+        }
+        else
+            Serial.printf("No product %d remain in stock\n", product_no);
+        refresh_vdisplay();
+    }
+}
+
+
